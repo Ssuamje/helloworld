b89295049178f799b582192d1a7c0ede92d97abe
Ssuamje <105692206+Ssuamje@users.noreply.github.com>
2022-06-29 16:18:32 +0900

    새로운 hello world 만들었음

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args){
         System.out.println("안녕 World");
-        System.out.println("HI 이건 VSCODE에서 수정한거야")
+        System.out.println("비겁하다 욕하지마 더러운 뒷골목을 헤매고 다녀도") // 추가된 문장
     }
 }
